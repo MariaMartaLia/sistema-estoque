7dcc3530c94f63d9c4751b5ba5356249a74a0384
Maria Marta <mms.liazinha.mms@gmail.com>
2026-02-19 07:28:54 -0300

    chore: removido arquivo duplicado visao_geral_md

--- db/schema.sql
+++ db/schema.sql
@@ -2,40 +2,40 @@ CREATE TABLE produto(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     descricao TEXT,
-    Quantidade_Atual INTEGER NOT NULL DEFAULT 0 CHECK (Quantidade_Atual >= 0),
-    Quantidade_Minima INTEGER NOT NULL DEFAULT 0 CHECK (Quantidade_Minima >= 0),
-    Data_Cadastro TIMESTAMP NOT NULL DEFAULT  CURRENT_TIMESTAMP,
-    Ativo BOOLEAN NOT NULL DEFAULT TRUE);
+    quantidade_atual INTEGER NOT NULL DEFAULT 0 CHECK (quantidade_atual >= 0),
+    quantidade_minima INTEGER NOT NULL DEFAULT 0 CHECK (quantidade_minima >= 0),
+    data_cadastro TIMESTAMP NOT NULL DEFAULT  CURRENT_TIMESTAMP,
+    ativo BOOLEAN NOT NULL DEFAULT TRUE);
 
 
-CREATE TABLE Movimentacao(
-    ID SERIAL PRIMARY KEY,
-    Produto_ID INTEGER NOT NULL,
-    Tipo_Movimentacao VARCHAR(10) NOT NULL CHECK (Tipo_Movimentacao IN('entrada', 'Sau=ida')),
-    Quantidade INTEGER NOT NULL CHECK (Quantidade > 0),
-    Data_Movimentacao TIMESTAMP NO NULL DEFAULT CURRENT_TIMESTAMP,
-    Observacao TEXT,
+CREATE TABLE movimentacao(
+    id SERIAL PRIMARY KEY,
+    produto_id INTEGER NOT NULL,
+    tipo_movimentacao VARCHAR(10) NOT NULL CHECK (tipo_movimentacao IN('entrada', 'saida')),
+    quantidade INTEGER NOT NULL CHECK (quantidade > 0),
+    data_movimentacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    observacao TEXT,
 
     CONSTRAINT fk_produto
-        FOREIGN KEY (Produto_ID)
-        REFERENCES produto(ID)
+        FOREIGN KEY (produto_id)
+        REFERENCES produto(id)
         ON DELETE RESTRICT
 );
 
-CREATE OR REPLACE FUNCTION Validar_Estoque()
+CREATE OR REPLACE FUNCTION validar_estoque()
 RETURNS TRIGGER AS $$
 DECLARE
-    Estoque_Atual INTEGER;
+    estoque_atual INTEGER;
 BEGIN
-    --Buscar qauntidade atual do produto
-    SELECT Quantidade_Atual
-    INTO Estoque_Atual
+    --Buscar quantidade atual do produto
+    SELECT quantidade_atual
+    INTO estoque_atual
     FROM produto
-    WHERE ID = NEW.Produto_ID;
+    WHERE id = NEW.produto_id;
 
     --Se for saída, Validar quantidade 
-    IF NEW.Tipo_Movimentacao = 'saida'THEN
-        IF NEW.Quantidade > Estoque_Atual THEN  
+    IF NEW.tipo_movimentacao = 'saida' THEN
+        IF NEW.quantidade > estoque_atual THEN  
             RAISE EXCEPTION 'Estoque insuficiente para realizar esta operação!';
             END IF;
         END IF;
@@ -43,7 +43,7 @@ BEGIN
     END;
     $$ LANGUAGE plpgsql;
 
-    CREATE TRIGGER Trigger_Validar_Estoque
-    BEFORE INSERT ON Movimentacao
+    CREATE TRIGGER trigger_validar_estoque
+    BEFORE INSERT ON movimentacao
     FOR EACH ROW
-    EXECUTE FUNCTION Validar_Estoque();
+    EXECUTE FUNCTION validar_estoque();
